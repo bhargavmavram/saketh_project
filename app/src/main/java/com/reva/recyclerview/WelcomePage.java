9cc36df575c871cd60119a75b97bdb0a2862f44c
sakethb10 <93110587+sakethb10@users.noreply.github.com>
2024-05-10 00:13:17 +0530

    Update WelcomePage.java
    Code update for profile image

--- app/src/main/java/com/reva/recyclerview/WelcomePage.java
+++ app/src/main/java/com/reva/recyclerview/WelcomePage.java
@@ -1,14 +1,25 @@
 package com.reva.recyclerview;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
 
 public class WelcomePage extends AppCompatActivity {
-    Button updateProfButton, signOutButton, deleteAccountButton, visitLibrary;
+    Button updateProfButton, signOutButton, deleteAccountButton, visitLibrary, selectImageButton;
+    ImageView profileImageView;
     DBHelper dbHelper;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +29,8 @@ public class WelcomePage extends AppCompatActivity {
         signOutButton = findViewById(R.id.btnSignOut);
         deleteAccountButton = findViewById(R.id.btnDeleteProfile);
         visitLibrary = findViewById(R.id.btnLibrary);
+        selectImageButton = findViewById(R.id.btnSelectImage);
+        profileImageView = findViewById(R.id.imgProfile);
         updateProfButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -46,6 +59,33 @@ public class WelcomePage extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+        selectImageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openGallery();
+            }
+        });
+    }
+
+    private void openGallery() {
+        Intent gallery = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI);
+        startActivityForResult(gallery, PICK_IMAGE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == PICK_IMAGE && resultCode == RESULT_OK && data != null) {
+            Uri imageUri = data.getData();
+            try {
+                InputStream inputStream = getContentResolver().openInputStream(imageUri);
+                Bitmap bitmap = BitmapFactory.decodeStream(inputStream);
+                profileImageView.setImageBitmap(bitmap);
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
     public void onBackPressed() {
